b208c9972b38c77079cb332e4b2555d32f16d44e
SamRanddals <bubblerbig120@gmail.com>
2025-02-07 14:12:44 -0500

    empieza el portfolio

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+// Apartado de proyectos, aqui se definira eventos de click que enviaran al resto de proyecto 
+
+let proyect1= document.querySelector(".proyecto1")
+let proyect2= document.querySelector(".proyecto2")
+let proyect3= document.querySelector(".proyecto3")
+let proyect4= document.querySelector(".proyecto4")
+let proyect5= document.querySelector(".proyecto5")
+
+
+proyect1.addEventListener("click",()=>{
+
+    window.open("https://samrandals.github.io/PinterestClon/","blank")
+})
+proyect2.addEventListener("click",()=>{
+
+    window.open("https://samrandals.github.io/practice.github.io/","blank")
+})
+proyect3.addEventListener("click",()=>{
+
+    window.open("https://co.pinterest.com/","blank")
+})
+proyect4.addEventListener("click",()=>{
+
+    window.open("https://co.pinterest.com/","blank")
+})
+proyect5.addEventListener("click",()=>{
+
+    window.open("https://co.pinterest.com/","blank")
+})
+
+
+
+
+//github href
+
+let github = document.querySelector(".github")
+
+
+github.addEventListener("click",()=>{
+
+    window.open("https://github.com/SamRandals","blank")
+})
